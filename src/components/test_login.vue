034bc45bdaebb48e879698b7f11340393e4c5df2
jinkyungkim12 <skwlsrud3@naver.com>
2022-12-13 16:56:55 +0900

    init

--- src/components/test_login.vue
+++ src/components/test_login.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="data_binding">
-    <form>
+  <div class="login">
+    <form v-on:submit.prevent="loginUser">
       <div class="container">
       <div class="text-center mb-3 mt-3"><h3><b>{{ msg }}</b></h3></div> 
         <div class="mb-3 row">
           <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="staticEmail" value="email@example.com">
+            <input type="text" class="form-control" id="staticEmail" ref="staticEmail" value="email@example.com">
           </div>
         </div>
         <div class="mb-3 row">
           <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword">
+            <input type="password" class="form-control" id="inputPassword" ref="inputPassword">
           </div>
         </div>
         <div class="mb-3 row justify-content-center">
           <button class="btn btn-dark" type="submit" id="loginBtn">Login</button>
         </div>
-      </div>
+      </div> 
     </form>
 
   </div>
@@ -26,7 +26,7 @@
 
 <script>
 export default {
-  name: 'test_data_binding',
+  name: 'test_login',
   props: {
     msg: String
   }
@@ -38,6 +38,7 @@ export default {
 //   },
 // }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
